4f007fbaf3eb283ac0cfe365df721af23f36b44c
HijazP <hanifaliramdani98@gmail.com>
2022-06-10 19:51:29 +0700

    Update

--- sketch.js
+++ sketch.js
@@ -1,36 +1,20 @@
-const settings = {
-  dimensions: [ 1600, 900 ]
-};
+var c = document.getElementById("myCanvas");
+var context = c.getContext("2d");
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+var width = 1600;
+var height = 900;
 
 context.fillStyle = 'white';
 context.fillRect(0, 0, width, height);
 
-    context.save();
-    context.translate(0, height);
-    context.scale(1, -1);
-    for (let index = 1; index <= 6; index++) {
-      context.beginPath();
-      context.moveTo(((width - (2 * border)) / 6 * index) + border, border);
-      context.lineTo(((width - (2 * border)) / 6 * index) + border, height - border);
-      context.strokeStyle = 'red';
-      context.stroke();
-      context.closePath();
-    }
-    context.restore();
+const border = 50;
 
-    context.save();
-    context.translate(0, height);
-    context.font = "28px Arial";
-    context.fillStyle = 'black';
-    for (let index = 1; index <= 6; index++) {
-      context.fillText(`${[index]}0`, ((width - (2 * border)) / 6 * index) + 10 + (border / 2), -15);
-    }
-    context.restore();
+context.beginPath();
+context.moveTo(border, border);
+context.lineTo(border, height - border);
+context.lineTo(width - border, height - border);
+context.stroke();
+context.closePath();
 
 context.save();
 context.translate(0, height);
@@ -54,12 +38,7 @@ for (let index = 1; index <= 7; index++) {
 }
 context.restore();
 
-    d3.csv("obese_nations_iso.csv", function (csv) {
-      csv.map(function (d) {
-        country.push(d.country);
-        female.push(+d.female);
-        male.push(+d.male);
-      })
+const maxGender = 70;
 
 var country = [];
 var female = [];
